bba31be411e400bef9acfc64f2d73d5a96bdff9a
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-05-02 21:59:40 +0400

    Create Burger test

---
+++ src/main/java/testData/TestNames.java
@@ -0,0 +1,7 @@
+package testData;
+
+public class TestNames {
+    public static String BUN_NAME = "Просто космос";
+    public static String FILLING_NAME = "Человеческая многоножка";
+    public static String SAUCE_NAME = "Нечеловеческая многоручка";
+}
---
+++ src/main/java/testData/TestPrices.java
@@ -0,0 +1,8 @@
+package testData;
+
+public class TestPrices {
+    public static float BUN_PRICE = 6.5f;
+    public static float FILLING_PRICE = 200f;
+    public static float SAUCE_PRICE = 45f;
+    public static float EXPECTED_BURGER_PRICE = BUN_PRICE * 2 + FILLING_PRICE + SAUCE_PRICE;
+}
